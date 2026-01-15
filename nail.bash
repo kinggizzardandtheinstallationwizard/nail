d5904150f20f920ebe8bee9272ebd4303244302a
kinggizzardandtheinstallationwizard <245943325+kinggizzardandtheinstallationwizard@users.noreply.github.com>
2026-01-14 21:40:25 -0600

    comments

--- nail.bash
+++ nail.bash
@@ -2,9 +2,12 @@ nail() {
 	# nail --- "nl plus tail"
 	# Equivalent to `wc -l`, excluding empty lines
 
+	# interactive/pipe mode
 	if (($# < 1)); then
 		local -i i
 		i=0
+
+		# increment only if read line is not blank
 		while read -r l; do
 			if [[ -n "$l" ]]; then
 				((i++))
@@ -14,6 +17,7 @@ nail() {
 		printf "${i}\n"
 	fi
 	
+	# file mode
 	local -i i
 	local -i lines
 	local -i total
@@ -24,11 +28,14 @@ nail() {
 	do
 		lines=`nl $arg | tail -n1 | awk '{print $1}'`
 
+		# when there are multiple files, start each line
+		# of output with a space to correctly imitate `wc -l`
 		if (($# > 1)); then
 			printf " "
 		fi
 
 		printf "${lines} ${arg}\n"
+
 		((i++))
 		total=${total}+${lines}
 	done
